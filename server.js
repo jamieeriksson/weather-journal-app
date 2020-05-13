99bfb5bd88e7f63c92d2339f615798df3ad36701
Jamie Eriksson <jamieeriksson@gmail.com>
2020-05-13 12:42:59 -0700

    Set up server, basic routes, empty object

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Empty JS object to act as endpoint for all routes
+const projectData = {};
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -9,8 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// Initialize main project folder
+app.use(express.static("website"));
+
 // Start server
 const port = 8000;
 const server = app.listen(port, () =>
   console.log(`Server started. Running on localhost: ${port}.`)
 );
+
+// Routes
+app.get("/weather", function (req, res) {
+  res.send("GET request to /weather");
+});
+
+app.post("/weather", function (req, res) {
+  res.send("POST request to /weather");
+});
